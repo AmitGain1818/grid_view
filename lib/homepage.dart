d3d52e0bc33e44f2b5daca0a548b74db130a2697
amitgain1818 <sampamondal75@gmail.com>
2022-01-10 16:02:28 +0600

    first commit

---
+++ lib/homepage.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+import '../main.dart';
+
+class GridViewNumberPage extends StatelessWidget {
+  final numbers = List.generate(21, (index) => '$index');
+  final controller = ScrollController();
+
+  @override
+  Widget build(BuildContext context) => Scaffold(
+        backgroundColor: Colors.black,
+        appBar: AppBar(
+          title: Center(child: Text('GridView')),
+        ),
+        body: buildGridView(),
+      );
+
+  Widget buildGridView() => GridView.builder(
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 3,
+          childAspectRatio: 1,
+          mainAxisSpacing: 8,
+          crossAxisSpacing: 8,
+        ),
+        padding: const EdgeInsets.all(4),
+        controller: controller,
+        itemCount: numbers.length,
+        itemBuilder: (context, index) {
+          final item = numbers[index];
+
+          return buildNumber(item);
+        },
+      );
+
+  Widget buildNumber(String number) => Container(
+        padding: EdgeInsets.all(16),
+        color: Colors.orange,
+        child: GridTile(
+          child: Center(
+            child: Text(
+              number,
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 48),
+              textAlign: TextAlign.center,
+            ),
+          ),
+        ),
+      );
+}
---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+import 'homepage.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) => MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'GridView',
+        theme: ThemeData(primarySwatch: Colors.deepOrange),
+        home: GridViewNumberPage(),
+      );
+}
